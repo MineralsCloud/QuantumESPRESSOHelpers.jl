55926c55093cdeedf7eefb45f57c28b38c09ef92
singularitti <singularitti@outlook.com>
2022-11-11 03:15:57 -0500

    Fix import in src/Inputs/Inputs.jl & reformat

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -18,22 +18,25 @@ function input_builder end
 # This is a helper function and should not be exported.
 function setfield_helper(terminal::TTYTerminal, nml::T) where {T<:Namelist}
     while true
-        print(terminal, BLUE_FG(qestring(nml)) |> string)
+        print(terminal, string(BLUE_FG(qestring(nml))))
         # It will continuously print until the user chooses `"no"`, i.e., he/she is satisfied.
         isdone = pairs((false, true))[request(
             terminal,
-            GREEN_FG("We generate an example `$(nameof(T))`. Want to change/add any field?") |>
-            string,
+            string(
+                GREEN_FG(
+                    "We generate an example `$(nameof(T))`. Want to change/add any field?"
+                ),
+            ),
             RadioMenu(["yes", "no"]),
         )]
         if !isdone
             while true
-                print(terminal, GREEN_FG("Type a field name: ") |> string)
-                field = strip(readline(terminal)) |> Symbol
+                print(terminal, string(GREEN_FG("Type a field name: ")))
+                field = Symbol(strip(readline(terminal)))
                 # Once a field successfully changes, go back to the above menu.
                 # The code will asks the user whether to change another field.
                 if hasfield(T, field)
-                    print(terminal, GREEN_FG("Type its value: ") |> string)
+                    print(terminal, string(GREEN_FG("Type its value: ")))
                     try
                         S = fieldtype(T, field)
                         nml = if S <: AbstractString
@@ -49,14 +52,14 @@ function setfield_helper(terminal::TTYTerminal, nml::T) where {T<:Namelist}
                         !isa(e, AssertionError) && rethrow(e)
                         println(
                             terminal,
-                            RED_FG("A wrong value is given to! Try a new one!") |> string,
+                            string(RED_FG("A wrong value is given to! Try a new one!")),
                         )
                         continue
                     end
                     break
                 end
                 # If the field has a wrong name, go back to `"Type a field name: "`.
-                println(terminal, RED_FG("Unknown field given! Try again!") |> string)
+                println(terminal, string(RED_FG("Unknown field given! Try again!")))
                 continue
             end
             continue
@@ -64,7 +67,7 @@ function setfield_helper(terminal::TTYTerminal, nml::T) where {T<:Namelist}
         break
     end
     return nml
-end # function setfield_helper
+end
 
 include("PWscf.jl")
 include("PHonon.jl")
