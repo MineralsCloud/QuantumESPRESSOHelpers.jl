c02960a3bb19f2ba10eb2779c9a6dba71b346155
singularitti <singularitti@outlook.com>
2022-11-11 02:21:53 -0500

    Update doc files

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,17 @@
 using QuantumESPRESSOHelpers
 using Documenter
 
+DocMeta.setdocmeta!(QuantumESPRESSOHelpers, :DocTestSetup, :(using QuantumESPRESSOHelpers); recursive=true)
+
 makedocs(;
     modules=[QuantumESPRESSOHelpers],
-    authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/MineralsCloud/QuantumESPRESSOHelpers.jl/blob/{commit}{path}#L{line}",
+    authors="singularitti <singularitti@outlook.com> and contributors",
+    repo="https://github.com/MineralsCloud/QuantumESPRESSOHelpers.jl/blob/{commit}{path}#{line}",
     sitename="QuantumESPRESSOHelpers.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://MineralsCloud.github.io/QuantumESPRESSOHelpers.jl",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
