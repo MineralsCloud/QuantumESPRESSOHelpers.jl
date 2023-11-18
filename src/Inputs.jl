2fc4450de8eb831f85d3674053f2043b31cebb1a
singularitti <singularitti@outlook.com>
2023-11-18 03:44:36 -0500

    Replace `Crayons.Box` with `Term` macros

--- src/Inputs.jl
+++ src/Inputs.jl
@@ -1,9 +1,8 @@
-using REPL.TerminalMenus: RadioMenu, request
-
-using Crayons.Box: BLUE_FG, GREEN_FG, RED_FG
-using QuantumESPRESSOBase: Namelist
+using AbInitioSoftwareBase: Namelist
+using Crayons.Box: BLUE_FG
 using REPL.TerminalMenus: RadioMenu, request, terminal
 using Setfield: PropertyLens, set
+using Term: @green, @red
 
 export build
 
@@ -12,25 +11,21 @@ function build end
 # This is a helper function and should not be exported.
 function setfield_helper(term::IO, nml::T) where {T<:Namelist}
     while true
-        print(term, string(BLUE_FG(nml)))
+        print(term, @green string(nml))
         # It will continuously print until the user chooses `"no"`, i.e., he/she is satisfied.
         isdone = pairs((false, true))[request(
             term,
-            string(
-                GREEN_FG(
-                    "We generate an example `$(nameof(T))`. Want to change/add any field?"
-                ),
-            ),
+            @green("We generate an example `$(nameof(T))`. Want to change/add any field?"),
             RadioMenu(["yes", "no"]),
         )]
         if !isdone
             while true
-                print(term, string(GREEN_FG("Type a field name: ")))
+                print(term, @green "Type a field name: ")
                 field = Symbol(strip(readline(term)))
                 # Once a field successfully changes, go back to the above menu.
                 # The code will asks the user whether to change another field.
                 if hasfield(T, field)
-                    print(term, string(GREEN_FG("Type its value: ")))
+                    print(term, @green "Type its value: ")
                     try
                         S = fieldtype(T, field)
                         nml = if S <: AbstractString
@@ -44,16 +39,13 @@ function setfield_helper(term::IO, nml::T) where {T<:Namelist}
                         end
                     catch e
                         !isa(e, AssertionError) && rethrow(e)
-                        println(
-                            term,
-                            string(RED_FG("A wrong value is given to! Try a new one!")),
-                        )
+                        println(term, @red "A wrong value is given to! Try a new one!")
                         continue
                     end
                     break
                 end
                 # If the field has a wrong name, go back to `"Type a field name: "`.
-                println(term, string(RED_FG("Unknown field given! Try again!")))
+                println(term, @red "Unknown field given! Try again!")
                 continue
             end
             continue
