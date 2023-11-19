08f86989317a020f872c8b30fe928e5b8f06ec1c
singularitti <singularitti@outlook.com>
2023-11-18 19:44:18 -0500

    Rename `term` to `io` in `build`

--- src/PHonon.jl
+++ src/PHonon.jl
@@ -12,7 +12,7 @@ using QuantumESPRESSOBase.PHonon:
 using REPL.TerminalMenus: RadioMenu, request
 using Term: @green
 
-using ..QuantumESPRESSOHelpers: YES_NO_MENU, help_set
+using ..QuantumESPRESSOHelpers: YES_NO_MENU, FieldSetter
 
 const EPSIL = pairs((false, true))
 const Q_IN_BAND_FORM = pairs((false, true))
@@ -22,87 +22,87 @@ const ASR = pairs(("no", "simple", "crystal", "one-dim", "zero-dim"))
 const Q_IN_CRYST_COORD = pairs((false, true))
 const NOSYM = pairs((false, true))
 
-function build(term::IO, ::Type{PhNamelist})
+function build(io::IO, ::Type{PhNamelist})
     print(
-        term,
+        io,
         @green "Please input the atomic mass [amu] of each atomic type `amass` (separated by spaces): "
     )
-    amass = map(Base.Fix1(parse, Float64), split(readline(term), " "; keepempty=false))
-    epsil = EPSIL[request(term, @green("Please select the `epsil`: "), YES_NO_MENU)]
+    amass = map(Base.Fix1(parse, Float64), split(readline(io), " "; keepempty=false))
+    epsil = EPSIL[request(io, @green("Please select the `epsil`: "), YES_NO_MENU)]
     q_in_band_form = Q_IN_BAND_FORM[request(
-        term, @green("Please select the `q_in_band_form`: "), YES_NO_MENU
+        io, @green("Please select the `q_in_band_form`: "), YES_NO_MENU
     )]
     print(
-        term,
+        io,
         @green "Please input parameters of the Monkhorst-Pack grid `nq` 1-3 (separated by spaces): "
     )
     nq1, nq2, nq3 = map(
-        Base.Fix1(parse, Float64), split(readline(term), " "; keepempty=false)
+        Base.Fix1(parse, Float64), split(readline(io), " "; keepempty=false)
     )
     print(
-        term,
+        io,
         @green "Please input parameters of the Monkhorst-Pack grid `nk` 1-3 (separated by spaces): "
     )
-    nk1, nk2, nk3 = map(Base.Fix1(parse, Int), split(readline(term), " "; keepempty=false))
-    print(term, @green "Please input offset `k` 1-3 (separated by spaces): ")
-    k1, k2, k3 = map(Base.Fix1(parse, Int), split(readline(term), " "; keepempty=false))
+    nk1, nk2, nk3 = map(Base.Fix1(parse, Int), split(readline(io), " "; keepempty=false))
+    print(io, @green "Please input offset `k` 1-3 (separated by spaces): ")
+    k1, k2, k3 = map(Base.Fix1(parse, Int), split(readline(io), " "; keepempty=false))
     ph = PhNamelist(;
         amass, epsil, q_in_band_form, nq1, nq2, nq3, nk1, nk2, nk3, k1, k2, k3
     )
-    return help_set(term, ph)
+    return FieldSetter()(io, ph)
 end
-function build(term::IO, ::Type{Q2rNamelist})
-    print(term, @green "name of input dynamical matrices `fildyn`: ")
-    fildyn = strip(readline(term))
-    print(term, @green "name of output force constants `flfrc`: ")
-    flfrc = strip(readline(term))
+function build(io::IO, ::Type{Q2rNamelist})
+    print(io, @green "name of input dynamical matrices `fildyn`: ")
+    fildyn = strip(readline(io))
+    print(io, @green "name of output force constants `flfrc`: ")
+    flfrc = strip(readline(io))
     zasr = ZASR[request(
-        term,
+        io,
         @green(
             "Please input the type of acoustic sum rules used for the Born effective charges `zasr`: "
         ),
         RadioMenu(ZASR; charset=:ascii),
     )]
     q2r = Q2rNamelist(; fildyn, flfrc, zasr)
-    return help_set(term, q2r)
+    return FieldSetter()(io, q2r)
 end
-function build(term::IO, ::Type{MatdynNamelist})
+function build(io::IO, ::Type{MatdynNamelist})
     dos = DOS[request(
-        term,
+        io,
         @green("Please select if calculate phonon density of states `dos`: "),
         YES_NO_MENU,
     )]
-    print(term, @green "Please input the energy step, in cm^(-1) `deltaE`: ")
-    deltaE = parse(Float64, readline(term))
+    print(io, @green "Please input the energy step, in cm^(-1) `deltaE`: ")
+    deltaE = parse(Float64, readline(io))
     print(
-        term,
+        io,
         @green "Please input uniform q-point grid for DOS calculation `nk` 1-3 (separated by spaces): "
     )
-    nk1, nk2, nk3 = map(Base.Fix1(parse, Int), split(readline(term), " "; keepempty=false))
+    nk1, nk2, nk3 = map(Base.Fix1(parse, Int), split(readline(io), " "; keepempty=false))
     asr = ASR[request(
-        term,
+        io,
         @green("Please input the type of acoustic sum rule `asr`: "),
         RadioMenu(ASR; charset=:ascii),
     )]
-    print(term, @green "name of output force constants `flfrc`: ")
-    flfrc = strip(readline(term))
-    print(term, @green "name of input dynamical matrices `fildyn`: ")
-    fildyn = strip(readline(term))
+    print(io, @green "name of output force constants `flfrc`: ")
+    flfrc = strip(readline(io))
+    print(io, @green "name of input dynamical matrices `fildyn`: ")
+    fildyn = strip(readline(io))
     print(
-        term,
+        io,
         @green "Please input the masses of atoms in the supercell (a.m.u.) `amass` (separated by spaces): "
     )
-    amass = map(Base.Fix1(parse, Float64), split(readline(term), " "; keepempty=false))
-    print(term, @green "Please input the number of atom types in the supercell `ntyp`: ")
-    ntyp = parse(Int, readline(term))
+    amass = map(Base.Fix1(parse, Float64), split(readline(io), " "; keepempty=false))
+    print(io, @green "Please input the number of atom types in the supercell `ntyp`: ")
+    ntyp = parse(Int, readline(io))
     q_in_band_form = Q_IN_BAND_FORM[request(
-        term, @green("Please select the `q_in_band_form`: "), YES_NO_MENU
+        io, @green("Please select the `q_in_band_form`: "), YES_NO_MENU
     )]
     q_in_cryst_coord = Q_IN_CRYST_COORD[request(
-        term, @green("Please select the `q_in_cryst_coord`: "), YES_NO_MENU
+        io, @green("Please select the `q_in_cryst_coord`: "), YES_NO_MENU
     )]
     nosym = NOSYM[request(
-        term,
+        io,
         @green("Please select if impose symmetry and time reversal `nosym`: "),
         YES_NO_MENU,
     )]
@@ -121,24 +121,22 @@ function build(term::IO, ::Type{MatdynNamelist})
         q_in_cryst_coord,
         nosym,
     )
-    return help_set(term, matdyn)
+    return FieldSetter()(io, matdyn)
 end
-function build(term::IO, ::Type{DynmatNamelist})
+function build(io::IO, ::Type{DynmatNamelist})
     asr = ASR[request(
-        term,
+        io,
         @green("Please select the type of acoustic sum rule `asr`: "),
         RadioMenu(ASR; charset=:ascii),
     )]
-    print(
-        term, @green "Please input mass for each atom type `amass` (separated by spaces): "
-    )
-    amass = map(Base.Fix1(parse, Float64), split(readline(term), " "; keepempty=false))
+    print(io, @green "Please input mass for each atom type `amass` (separated by spaces): ")
+    amass = map(Base.Fix1(parse, Float64), split(readline(io), " "; keepempty=false))
     dynmat = DynmatNamelist(; asr, amass)
-    return help_set(term, dynmat)
+    return FieldSetter()(io, dynmat)
 end
-build(term::IO, ::Type{PhInput}) = PhInput(build(term, PhNamelist))
-build(term::IO, ::Type{Q2rInput}) = Q2rInput(build(term, Q2rNamelist))
-build(term::IO, ::Type{MatdynInput}) = MatdynInput(build(term, MatdynNamelist))
-build(term::IO, ::Type{DynmatInput}) = DynmatInput(build(term, DynmatNamelist))
+build(io::IO, ::Type{PhInput}) = PhInput(build(io, PhNamelist))
+build(io::IO, ::Type{Q2rInput}) = Q2rInput(build(io, Q2rNamelist))
+build(io::IO, ::Type{MatdynInput}) = MatdynInput(build(io, MatdynNamelist))
+build(io::IO, ::Type{DynmatInput}) = DynmatInput(build(io, DynmatNamelist))
 
 end
