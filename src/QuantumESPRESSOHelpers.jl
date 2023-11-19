1f3aa481197a079e7c571428bd918c3a186825de
singularitti <singularitti@outlook.com>
2023-11-18 19:38:06 -0500

    Define `QuantumESPRESSOHelper`

--- src/QuantumESPRESSOHelpers.jl
+++ src/QuantumESPRESSOHelpers.jl
@@ -4,6 +4,8 @@ using REPL.TerminalMenus: RadioMenu
 
 const YES_NO_MENU = RadioMenu(Base.vect("yes", "no"); charset=:ascii)
 
+abstract type QuantumESPRESSOHelper end
+
 include("input.jl")
 include("PWscf/PWscf.jl")
 include("PHonon.jl")
